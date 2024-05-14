9c278b030917835b00d867414c204bb202de53a1
dimashbalabek <dimashbalabek0@gmail.com>
2024-05-14 14:11:57 +0600

    added files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# flutter_topic9
+![image](https://github.com/dimashbalabek/flutter_topic78/assets/144226438/59e1433e-ec79-463d-9594-de53aac76119)
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:video/pages/register_form_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Register Form Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RegisterFormPage(),
+    );
+  }
+}
